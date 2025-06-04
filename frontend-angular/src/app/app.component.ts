c56f0928b977df1b93714ee543eea1139412e699
Philipp Schlosser <104062012+FidgetyPhil@users.noreply.github.com>
2025-06-04 00:04:43 +0000

    Finalized Dark/Bright Mode Button for Angular

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -17,6 +17,7 @@ type Config = {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
   imports: [CommonModule, FormsModule, HttpClientModule]
 })
 export class AppComponent implements OnInit {
